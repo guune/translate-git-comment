a474b72163f23d6a300f85cc6d5504300054e883
gyuhong <qjwb2@naver.com>
2024-05-29 19:41:43 +0900

    ✅ : git 분석할 테스트 문서 추가

---
+++ test/test1.h
@@ -0,0 +1,9 @@
+/* Register a new codec search function.
+
+   As side effect, this tries to load the encodings package, if not
+   yet done, to make sure that it is always first in the list of
+   search functions.
+
+   The search_function's refcount is incremented by this function. */
+
+PyAPI_FUNC(int) PyCodec_Register(PyObject *search_function);
