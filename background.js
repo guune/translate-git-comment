4b00ea5bb0d4fd420ffab407cc320d5c9d25f8b2
gyuhong <qjwb2@naver.com>
2024-05-29 22:07:20 +0900

    🚧 : 주석 밑에 appendChild 까지 성공

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-function writeCommentData() {
+function addCommentData() {
     const lines = document.querySelectorAll('span.pl-c');
     if (lines) {
         const data = {};
@@ -14,7 +14,12 @@ function writeCommentData() {
             const parentId = parent ? parent.getAttribute('id') : null;
             data[`${parentId}`] = `${dataCodeText}`
         }
-        console.log(data);
+        for (let id in data) {
+            let p = document.createElement('p');
+            p.textContent = data[id];
+            let el = document.querySelector(`#${id}`);
+            el.appendChild(p);
+        }
     }
 }
 
@@ -23,17 +28,23 @@ chrome.action.onClicked.addListener(async (tab) => {
     if (tab.url.startsWith(github)) {
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
         const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
         await chrome.action.setBadgeText({
             tabId: tab.id,
             text: nextState
         });
+
         if (nextState === 'ON') {
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
-                func: writeCommentData,
+                func: addCommentData,
             });
         } else if (nextState === 'OFF') {
             // 구현중...
+            // chrome.scripting.executeScript({
+            //     target: { tabId: tab.id },
+            //     func: deleteCommentData,
+            // });
         }
     }
 });
