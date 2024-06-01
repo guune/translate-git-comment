2d7e350fcd2723e7567f78c648f98477e0461967
guu <guuness@naver.com>
2024-06-02 03:42:24 +0900

    ✨ : 간단한 서버에 postData 구현

--- background.js
+++ background.js
@@ -4,14 +4,32 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-function addCommentData() {
+
+async function addCommentData() {
+    // POST 메서드 구현 예제
+    async function postData(url = "", data = {}) {
+        const response = await fetch(url, {
+            method: "POST", // *GET, POST, PUT, DELETE 등
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
+        });
+        return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
+    }
+
     const lines = document.querySelectorAll('span.pl-c');
     if (lines) {
         for (let line of lines) {
-            // let originalComment = line.getAttribute('data-code-text');
-            // let translatedComment = 이걸 구현해야함
-            // line.setAttribute('data-code-text', translatedComment);
-            line.setAttribute('data-code-text', "test!!!");
+            // todo: 한줄 한줄 번역하니깐 느림. 파싱이 필요해 보임
+            let translatedCommentJson = await postData("http://localhost:8080/translate/", { text: `${line.innerText}` });
+            line.innerHTML = translatedCommentJson["translatedComment"];
+            // line.setAttribute('data-code-text', "test!!!");
         }
     }
 }
